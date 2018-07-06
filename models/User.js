cb4ed4de482c1ba52f40d3c129af404775a8112f
Colin MJ <colinmj@telus.net>
2018-07-05 21:01:23 -0700

    Created user login logic

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//creating a schema
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+var User = mongoose.model('users', UserSchema);
+
+module.exports = {
+  User
+};
+
+// module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+//load user model
+const { User } = require('../../models/User');
 
 // we already have api/users accounted for
 
@@ -10,6 +15,73 @@ const router = express.Router();
 router.get('/test', (req, res) => {
   res.json({
     msg: 'Users is working'
+  });
+});
+
+//@get    GET api/users/register
+//@desc   Register a user
+//@access public
+
+router.post('/register', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: 'Email already exists'
+      });
+    }
+  });
+  const avatar = gravatar.url(req.body.email, {
+    s: 200, // size
+    rating: 'pg',
+    default: 'mm'
+  });
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    avatar,
+    password: req.body.password
+  });
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser
+        .save()
+        .then(user => {
+          res.json(user);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  });
+});
+
+//@get    GET api/users/login
+//@desc   login a user/ returning token
+//@access public
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find the user by email
+  User.findOne({
+    email
+  }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: 'Success' });
+      } else {
+        return res.status(400).json({ password: 'Password is incorrect' });
+      }
+    });
   });
 });
 
