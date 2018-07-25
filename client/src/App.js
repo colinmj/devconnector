ae155790c57c4755cd90b8ed896ccc872f7f39eb
Colin MJ <colinmj@telus.net>
2018-07-24 23:09:08 -0700

    Adding likes and comments

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/notfound/NotFound';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
@@ -82,6 +84,8 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+                <PrivateRoute exact path="/post/:id" component={Post} />
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
---
+++ client/src/components/post/Post.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Loader from '../common/Loader';
+import PostItem from '../posts/PostItem';
+import { getPost } from '../../actions/postActions';
+import { Link } from 'react-router-dom';
+import CommentForm from './CommentForm';
+
+class Post extends Component {
+  componentDidMount() {
+    this.props.getPost(this.props.match.params.id);
+  }
+  render() {
+    const { post, loading } = this.props.post;
+    let postContent;
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Loader />;
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
+        </div>
+      );
+    }
+    return (
+      <div className="post">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back To Feed
+              </Link>
+              {postContent}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Post.propTypes = {
+  post: PropTypes.object.isRequired,
+  getPost: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Post);
